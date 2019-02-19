33aea02301b69deb93b598aaa0bf60505ec60fb1
Fernando Mendes <fernando@mendes.codes>
2019-02-19 23:57:39 +0000

    Prepare for production deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,15 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :paas, PaasWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: System.get_env("PORT")],
+  url: [host: System.get_env("HOST"), port: System.get_env("PORT")],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :paas, Pass.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +74,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
